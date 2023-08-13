81b65117fd29ad6415bb8e2de27b10aa5dc06924
Njerimacharia98 <njerimacharia05@gmail.com>
2023-08-13 12:45:54 +0300

    newfile

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,22 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+      end
+    
+      def show
+        plant = Plant.find(params[:id])
+        render json: plant
+      end
+    
+      def create
+        plant = Plant.create(plant_params)
+        render json: plant
+      end
+    
+      private
+    
+      def plant_params
+        params.require(:plant).permit(:name, :image, :price)
+      end
+end
